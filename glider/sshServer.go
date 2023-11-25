f51505d56e9410f36b4d4611be244c0197fc9564
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-11-25 13:50:07 +0100

    initial commit

---
+++ glider/sshServer.go
@@ -0,0 +1,21 @@
+// https://github.com/gliderlabs/ssh/blob/master/_examples/ssh-simple/simple.go
+//
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/gliderlabs/ssh"
+)
+
+func main() {
+	ssh.Handle(func(s ssh.Session) {
+		io.WriteString(s, fmt.Sprintf("Hello %s\n", s.User()))
+	})
+
+	log.Println("starting ssh server on port 2222...")
+	log.Fatal(ssh.ListenAndServe(":2222", nil))
+}
